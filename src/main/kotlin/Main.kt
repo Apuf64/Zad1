dc7b4e914f3d0e685dd460453f7bff8170bd3d3f
1297-22 <apuf2006@yandex.ru>
2024-09-07 19:06:33 +0300

    init

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,67 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+fun main() {
+    print("Введите строку:")
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    val inputV = readln()
+
+    var result = ""
+
+    var countC = 1
+
+    var previousC = inputV[0]
+
+    for (i in 1 .. inputV.length - 1) {
+
+        if (inputV[i] == previousC) {
+
+            countC++
+
+        }
+        else{
+            if (countC > 1) {
+
+                result = result + previousC + countC
+
+            }
+            else {
+
+                result = result + previousC
+
+            }
+
+            countC = 1
+            previousC = inputV[i]
+
+        }
+
+        if (countC > 1) {
+
+            result = result + previousC + countC
+
+        }
+        else {
+
+            result = result + previousC
+
+        }
+
+        countC = 1
+
+        previousC = inputV[i]
+
+    }
+
+    if (countC > 1){
+
+        result = result + previousC + countC
+
+    }
+
+    else{
+
+        result = result + previousC
+
+    }
+
+    print(result)
+
 }
